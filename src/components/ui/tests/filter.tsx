a0cae64813dba28e3e84cadebcebfb237a92fec9
MarufaKompass <Marufa@hellokompass.com>
2025-12-18 17:17:06 +0600

    added

--- src/components/ui/tests/filter.tsx
+++ src/components/ui/tests/filter.tsx
@@ -1,16 +1,16 @@
 import { Button } from '../button'
 
-export default function Filter({setOpen}) {
+export default function Filter({ setOpen }: { setOpen: (open: boolean) => void }) {
   return (
- <section>
-    <div className='flex justify-between mb-10'>
+    <section>
+      <div className='flex justify-between mb-10'>
         <div>
-          <Button className='text-white bg-transparent border border-[#F6F4F20D] px-8 '   onClick={() => setOpen(true)}  style={{ boxShadow: "inset 0 1px 2px rgba(246, 244, 242, 0.45)" }}>Filter</Button>
+          <Button className='text-white bg-transparent border border-[#F6F4F20D] px-8 ' onClick={() => setOpen(true)} style={{ boxShadow: "inset 0 1px 2px rgba(246, 244, 242, 0.45)" }}>Filter</Button>
         </div>
         <div>
           <Button className='text-white bg-transparent border border-[#F6F4F20D] px-8' style={{ boxShadow: "inset 0 1px 2px rgba(246, 244, 242, 0.45)" }}>Practice</Button>
         </div>
-    </div>
- </section>
+      </div>
+    </section>
   )
 }
